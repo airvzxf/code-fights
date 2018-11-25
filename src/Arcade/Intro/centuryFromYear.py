ea66d09db4e2fe0e60d6db9c9c05a43fe3eada5d
Israel Roldan <israel.alberto.rv@gmail.com>
2018-11-25 00:43:56 +0000

    Added test for Arcade -> Intro -> level 2

---
+++ src/Arcade/Intro/centuryFromYear.py
@@ -0,0 +1,14 @@
+from math import ceil
+
+def centuryFromYear(year):
+    decimal_year = year / 100
+    return ceil(decimal_year)
+
+print(centuryFromYear(1905), 20)
+print(centuryFromYear(1700), 17)
+print(centuryFromYear(1988), 20)
+print(centuryFromYear(200), 2)
+print(centuryFromYear(374), 4)
+print(centuryFromYear(45), 1)
+print(centuryFromYear(2001))
+print(centuryFromYear(2001))

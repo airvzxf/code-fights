91435f9d9125795ec88e6e79aaeb4b33933363a0
Israel Roldan <israel.alberto.rv@gmail.com>
2018-12-01 21:09:00 +0000

    Added the largestNumber into the The Core section in Arcade.

---
+++ src/Arcade/TheCore/largestNumber.py
@@ -0,0 +1,9 @@
+def largestNumber(n):
+    return int("9" * n)
+
+
+assert largestNumber(2) == 99
+assert largestNumber(1) == 9
+assert largestNumber(7) == 9999999
+assert largestNumber(4) == 9999
+assert largestNumber(3) == 999
